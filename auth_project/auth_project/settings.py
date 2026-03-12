8779f338d48a60ea4d65e39f96833813dba59544
Khushi Bansal <bansalkhushi880@gmail.com>
2026-03-12 15:06:46 +0530

    Fix allowed hosts for Render

--- auth_project/auth_project/settings.py
+++ auth_project/auth_project/settings.py
@@ -10,8 +10,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = True
-
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [
+    "authentication-system-pdhc.onrender.com",
+    "localhost",
+    "127.0.0.1"
+]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
