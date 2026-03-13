d7488d3866997d86299b49604f7784a62851518d
Khushi Bansal <bansalkhushi880@gmail.com>
2026-03-13 22:07:24 +0530

    Add build.sh

--- auth_project/build.sh
+++ auth_project/build.sh
@@ -1,14 +1,6 @@
-#!/usr/bin/env bash
-
 set -o errexit
-
-# React build
 npm install --prefix frontend
 npm run build --prefix frontend
-
-# Python dependencies
 pip install -r requirements.txt
-
-# Django
 python manage.py collectstatic --noinput
 python manage.py migrate
