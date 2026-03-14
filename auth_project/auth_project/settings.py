d7dffa7997085fd7651d2f519790ecd9f3fc602a
Khushi Bansal <bansalkhushi880@gmail.com>
2026-03-14 17:20:20 +0530

    fix jwt cookie issue

--- auth_project/auth_project/settings.py
+++ auth_project/auth_project/settings.py
@@ -142,6 +142,7 @@ LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
+
 USE_TZ = True
 
 
@@ -168,8 +169,20 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # CORS Settings
 # ======================
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "https://auth-system-au1r.onrender.com",
+]
+
 CORS_ALLOW_CREDENTIALS = True
+
+
+# ======================
+# CSRF Trusted Origins
+# ======================
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://auth-system-au1r.onrender.com"
+]
 
 
 # ======================
@@ -180,5 +193,5 @@ AUTH_COOKIE_ACCESS = 'access_token'
 AUTH_COOKIE_REFRESH = 'refresh_token'
 
 AUTH_COOKIE_HTTP_ONLY = True
-AUTH_COOKIE_SECURE = False
-AUTH_COOKIE_SAMESITE = 'Lax'
+AUTH_COOKIE_SECURE = True
+AUTH_COOKIE_SAMESITE = 'None'
